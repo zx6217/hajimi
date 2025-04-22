5d5f2bc1af14f7d70267ae6cf216a3c2d774a057
warming-afternoon <97532589+warming-afternoon@users.noreply.github.com>
2025-04-23 01:15:35 +0800

    fix: 修复假流式输出格式
    使假流式输出格式能被cherryStudio识别

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -7,6 +7,6 @@ from app.utils.rate_limiting import protect_from_abuse
 from app.utils.cache import ResponseCacheManager, generate_cache_key
 from app.utils.request import ActiveRequestsManager
 from app.utils.stats import clean_expired_stats, update_api_call_stats
-from app.utils.response import create_chat_response, create_error_response, create_response, handle_exception
+from app.utils.response import handle_exception
 from app.utils.version import check_version
 from app.utils.maintenance import handle_exception, schedule_cache_cleanup
