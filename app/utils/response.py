bee4ae469281cbfda76fb161828c51c7aed12373
warming-afternoon <97532589+warming-afternoon@users.noreply.github.com>
2025-05-01 19:39:54 +0800

    refactor: 移除openAI_nonstream_response的使用

--- app/utils/response.py
+++ app/utils/response.py
@@ -23,30 +23,30 @@ def openAI_stream_chunk(model="gemini",content=None,finish_reason=None,total_tok
 
 
 # 已废弃，将在后续版本中删除
-def openAI_nonstream_response(response):
-    """
-    使用 gemini 非流式响应对象(提取后),
-    创建 OpenAI 非流式标准响应对象
-    """
-    return {
-        "id": f"chatcmpl-{int(time.time()*1000)}",
-        "object": "chat.completion",
-        "created": int(time.time()),
-        "model": response.model,
-        "choices": [{
-            "index": 0, 
-            "message": {
-                "role": "assistant", 
-                "content": response.text
-            }, 
-            "finish_reason": "stop"
-        }],
-        "usage": {
-            "prompt_tokens": response.prompt_token_count,
-            "completion_tokens": response.candidates_token_count,
-            "total_tokens": response.total_token_count
-        }
-    }
+# def openAI_nonstream_response(response):
+#     """
+#     使用 gemini 非流式响应对象(提取后),
+#     创建 OpenAI 非流式标准响应对象
+#     """
+#     return {
+#         "id": f"chatcmpl-{int(time.time()*1000)}",
+#         "object": "chat.completion",
+#         "created": int(time.time()),
+#         "model": response.model,
+#         "choices": [{
+#             "index": 0, 
+#             "message": {
+#                 "role": "assistant", 
+#                 "content": response.text
+#             }, 
+#             "finish_reason": "stop"
+#         }],
+#         "usage": {
+#             "prompt_tokens": response.prompt_token_count,
+#             "completion_tokens": response.candidates_token_count,
+#             "total_tokens": response.total_token_count
+#         }
+#     }
 
 def openAI_from_Gemini(response,stream=True):
     """
